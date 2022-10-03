beb88869eae09e1453ff2fb6f91893a189245ce0
Кушнаренко Григорий <Gr4n777.yandex.ru>
2022-10-03 20:47:24 +0300

    ДЗ Массивы и функции

---
+++ Задача_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 100)
+{
+    number /= 10;
+    result = number % 10;
+}
+Console.Write("Вторая цифра числа: " + result);
---
+++ Задача_3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.*/
+
+Console.Write("Напишите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 5)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
